deaaaf73576949feefc6f7161b0410e64649726f
MarcAdrn <114600288+MarcAdrn@users.noreply.github.com>
2022-09-28 23:28:59 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Activity 1</title>
+</head>
+<body>
+<p>Marc Adrian Bautista</p><br>
+<p>BSIT 4A</p><br>
+<p>System Integration and Architecture</p><br>
+
+</body>
+</html>
